93265c01626468aa1ffac330ce7617e1d4b7cd2d
Ming Chung Ou <mingchungou@gmail.com>
2017-04-13 21:20:55 -0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,136 @@
+module.exports = function(grunt) {
+    /*grunt.registerTask("speak", function() {
+        console.log("I'm speaking");
+    });
+
+    grunt.registerTask("yell", function() {
+        console.log("I'm yelling");
+    });
+
+    grunt.registerTask("both", ["speak", "yell"]);
+
+    grunt.registerTask("default", ["both"]);*/
+
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: "\n\n\n"
+            },
+            js: {
+                src: [
+                    "js/jquery-3.2.0.min.js",
+                    "js/tether.min.js",
+                    "js/bootstrap.min.js",
+                    "js/owl.carousel.min.js",
+                    "js/wow.min.js",
+                    "js/smooth-scroll.min.js",
+                    "js/myScript.js"
+                ],
+                dest: "dist/js/app.js"
+            },
+            css: {
+                src: [
+                    "css/font-awesome.min.css",
+                    "css/owl.carousel.min.css",
+                    "css/owl.theme.default.min.css",
+                    "css/bootstrap.min.css",
+                    "css/animate.min.css",
+                    "css/myStyle.css"
+                ],
+                dest: "dist/css/style.css"
+            }
+        },
+        watch: {
+            js: {
+                files: ["js/**/*.js"],
+                tasks: ["concat:js"]
+            },
+            css: {
+                files: ["css/**/*.css"],
+                tasks: ["concat:css"]
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    "css/style.min.css": [
+                        "temp/font-awesome.min.css",
+                        "css/owl.carousel.min.css",
+                        "css/owl.theme.default.min.css",
+                        "css/bootstrap.min.css",
+                        "css/animate.min.css",
+                        "css/myStyle.css"
+                    ]
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    "js/script.min.js": [
+                        "js/jquery-3.2.0.min.js",
+                        "js/tether.min.js",
+                        "js/bootstrap.min.js",
+                        "js/owl.carousel.min.js",
+                        "js/wow.min.js",
+                        "js/smooth-scroll.min.js",
+                        "js/myScript.js"
+                    ]
+                }
+            }
+        },
+        imageEmbed: {
+            dist: {
+                src: "css/font-awesome.min.css",
+                dest: "temp/font-awesome.min.css",
+                options: {
+                    deleteAfterEncoding : false,
+                    maxImageSize: 0
+                }
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'scss',
+                    cssDir: 'dist'
+                }
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: ["temp"]
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dest: "scss",
+                    src: [
+                        "index.html"
+                    ]
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks("grunt-image-embed");
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask("default", [
+        "imageEmbed",
+        "cssmin",
+        "uglify",
+        "clean"
+    ]);
+};
